12226bf973316b85da6688056577e600fd55db95
yk2217 <yahiyakhan2217@gmail.com>
2020-07-12 16:37:29 +0530

    modified css

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const navSlide = () => {
-    const bars = document.querySelector(".bars");
-    const nav = document.querySelector(".nav-links");
-    bars.addEventListener('click', () => {
-        nav.classList.toggle("nav-active")
-    });
-}
-navSlide();
+// const navSlide = () => {
+//     const bars = document.querySelector(".bars");
+//     const nav = document.querySelector(".nav-links");
+//     bars.addEventListener('click', () => {
+//         nav.classList.toggle("nav-active")
+//     });
+// }
+// navSlide();
