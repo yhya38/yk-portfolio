d458d25b2b342def7de46f5d6df15ba08a81bce6
yk2217 <yahiyakhan2217@gmail.com>
2020-07-13 00:57:47 +0530

    modification of navbar completed

--- main.js
+++
@@ -1,8 +0,0 @@
-// const navSlide = () => {
-//     const bars = document.querySelector(".bars");
-//     const nav = document.querySelector(".nav-links");
-//     bars.addEventListener('click', () => {
-//         nav.classList.toggle("nav-active")
-//     });
-// }
-// navSlide();
